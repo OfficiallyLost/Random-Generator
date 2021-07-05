601118a3f274e3797b5e3a7f1146d0ec1e10639c
haseeb <farooqhaseeb49@gmail.com>
2021-07-05 14:03:48 +0100

    fixes

--- src/lib/index.js
+++ src/lib/index.js
@@ -5,8 +5,7 @@ const Core = require('./core/Class');
 
 class RandomGenerator {
 	constructor(options) {
-		console.log(this);
-		this.core = new Core();
+		this.core = new Core(this);
 		this.names = {
 			first: '',
 			middle: this.core.random.element(middleNames),
@@ -24,37 +23,32 @@ class RandomGenerator {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param {String} gender The gender of the person (male, female, any)
 	 * @param {?Boolean} first Either true or false, if you want the first name or not
 	 * @param {?Boolean} middle Either true or false, if you want middle first name or not
 	 * @param {?Boolean} last Either true or false, if you want the last name or not
 	 * @returns A random name based on the settings provided
 	*/
-
 	/**
-	 * 
+	 *
 	 * @param {?Number} lowest The lowest number
 	 * @param {?Number} highest The highest number
 	 * @returns A random number in the given parameters
 	 */
-
 	number(lowest, highest) {
 		if (!Number(lowest) || !Number(highest))
 			throw new Error('Both parameters must be of value of Number or you must add a lowest / highest number');
 		return Math.floor(Math.random() * highest + lowest);
 	}
-
 	get name() {
 		if (!this.options.gender) throw new Error('You must specify the gender (male, female, any)');
 		if (!this.options.first) this.options.first = false;
 		if (this.options.middle) this.options.middle = true;
 		if (this.options.last) this.options.last = true;
-
 		let randomFirst = this.names.first;
 		const randomMiddle = this.names.middle;
 		const randomLast = this.names.last;
-
 		if (this.options.gender === 'male') {
 			randomFirst = this.core.random.element(firstNames.male);
 			if (this.options.first && this.options.middle && this.options.last)
@@ -80,7 +74,6 @@ class RandomGenerator {
 			if (this.options.first && !this.options.middle && !this.options.last) return randomFirst;
 		} else throw new Error('That is not a valid gender (male, female, any)');
 	}
-
 	get profile() {}
 }
 
